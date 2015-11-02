7e0ab80806f3d4226d3c5f8d7a71c4401ebbcb50
Sam Sabra <samsabra@gmail.com>
2015-11-02 04:37:13 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ $app = new \Slim\Slim();
 
 $app->get('/', function () {
     echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . "\n";
-    echo "Too much awesomeness!"
+    echo "Too much awesomeness!";
 });
 
 $app->run();
